55c72e1bd1f3f623b57827cf5cca5dbfbf380c79
nicopaolillo <nico_paolillo.emanuel@hotmail.com>
2026-06-11 20:22:42 -0300

    refactor: migrar estado global a GameContext

--- game.py
+++ game.py
@@ -168,47 +168,21 @@ def opciones(cuadradoEnX,cuadradoEnY):
         pygame.display.update()
         screen.blit(opcionesFondo, (0,0))
 
-def init():
-    global tank_red_list
-    global tank_green_list
-    global shoot_list
-    global crash_list
-    global apoyo_list
-    global all_sprites
-    global player
-    global game_over
-    global pause
-    #creación de listas
-    tank_red_list = pygame.sprite.Group()
-    tank_green_list = pygame.sprite.Group()
-    shoot_list = pygame.sprite.Group()
-    crash_list = pygame.sprite.Group()
-    apoyo_list=pygame.sprite.Group()
-    all_sprites = pygame.sprite.Group()
-    #instanciación del jugador con clase player
-    player = Player()
-
-    #instanciación y guardado de tanques verdes
-    for i in range(3):
-        tank_green = Tank_green()
-        tank_green_list.add(tank_green)
-        all_sprites.add(tank_green)
-
-    #instanciación y guardado de tanques rojos
-    for i in range(5):
-        tank_red = Tank()    
-        tank_red_list.add(tank_red)
-        all_sprites.add(tank_red)
-
-    #guardo el sprite del tanque una lista
-    all_sprites.add(player)
-
-    #Condición para que corra el juego!!!
-    game_over = False
 
 #Bucle principal...................................................................../
 enProceso = True
 def game():
+    player = context.player
+
+    all_sprites = context.all_sprites
+
+    tank_red_list = context.tank_red_list
+    tank_green_list = context.tank_green_list
+
+    shoot_list = context.shoot_list
+    apoyo_list = context.apoyo_list
+    crash_list = context.crash_list
+
     player.hp=200
     player.nivel=1
     player.misiles=3
@@ -443,6 +417,6 @@ def game():
         
 while(enProceso):
     pygame.time.wait(3200)
-    init()
+    context = GameContext()
     menu()
     game()
